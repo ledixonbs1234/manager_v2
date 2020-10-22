c2c49111185a70fce3a75f7eaa4d954c1679f099
xon <ledixonbs12345@gmail.com>
2020-10-22 19:43:00 +0700

    add new float button

--- lib/app/modules/read/read_view.dart
+++ lib/app/modules/read/read_view.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:manager_v2/app/modules/chapter/chapter_controller.dart';
@@ -15,13 +16,22 @@ class ReadView extends GetView<ReadController> {
           title: Text('ReadView'),
           centerTitle: true,
         ),
-        body: Obx(() => !controller.isShow.value
-            ? CircularProgressIndicator()
-            : ListView(
-                physics: const AlwaysScrollableScrollPhysics(),
-                children: controller.pages
-                    .map((page) => _imageCell(page))
-                    .toList())));
+        floatingActionButton: FabCircularMenu(
+          children: [
+            IconButton(icon: Icon(Icons.one_k), onPressed: () {}),
+            IconButton(icon: Icon(Icons.ac_unit), onPressed: () {})
+          ],
+        ),
+        body: GestureDetector(
+          onLongPress: () {},
+          child: Obx(() => !controller.isShow.value
+              ? CircularProgressIndicator()
+              : ListView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  children: controller.pages
+                      .map((page) => _imageCell(page))
+                      .toList())),
+        ));
   }
 
   Widget _imageCell(PageModel imageUrl) {
@@ -31,9 +41,9 @@ class ReadView extends GetView<ReadController> {
             : CachedNetworkImage(
                 imageUrl: imageUrl.urlPath,
                 httpHeaders: {'referer': 'nettruyen'},
-          progressIndicatorBuilder: (context, url, downloadProgress) =>
-              CircularProgressIndicator(value: downloadProgress.progress),
-          errorWidget: (context, url, error) => Icon(Icons.error),
+                progressIndicatorBuilder: (context, url, downloadProgress) =>
+                    CircularProgressIndicator(value: downloadProgress.progress),
+                errorWidget: (context, url, error) => Icon(Icons.error),
               ));
   }
 }
